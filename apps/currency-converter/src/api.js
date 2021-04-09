6d7573965588c08b33565111d66a73d16ad3afe9
reslear <reslear@gmail.com>
2021-04-09 20:02:18 +0300

    refactor(currency-converter): change api source

--- apps/currency-converter/src/api.js
+++ apps/currency-converter/src/api.js
@@ -2,9 +2,11 @@ import axios from "axios";
 
 export async function getExchange(base = "USD") {
   try {
-    const { data } = await axios.get(
-      `http://api.exchangeratesapi.io/v1/latest?base=${base}&access_key=6a869760ead7263759b3cb09450189cb`
-    );
+    const { data } = await axios.get(`https://api.openrates.io/latest`, {
+      params: {
+        base
+      }
+    });
     return data ? { rates: data.rates, base: data.base } : false;
   } catch (error) {
     console.error(error);
--- apps/currency-converter/vue.config.js
+++ apps/currency-converter/vue.config.js
@@ -1,5 +1,13 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/demo/portfolio/examples/currency-converter/'
-      : '/'
-  }
+  publicPath:
+    process.env.NODE_ENV === "production"
+      ? "/demo/portfolio/examples/currency-converter/"
+      : "/",
+  filenameHashing: false,
+  configureWebpack: {
+    optimization: {
+      splitChunks: false
+    }
+  },
+  productionSourceMap: false
+};
