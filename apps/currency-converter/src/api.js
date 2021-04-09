9ad7aa2f3ce38ad7b73a3cc089cd11389a47f5a8
reslear <reslear@gmail.com>
2021-04-09 18:52:23 +0300

    refactor(currency-converter): add access_key

--- apps/currency-converter/src/api.js
+++ apps/currency-converter/src/api.js
@@ -1,10 +1,12 @@
-import axios from 'axios';
+import axios from "axios";
 
-export async function getExchange(base = 'USD') {
-    try {
-        const {data} = await axios('https://api.exchangeratesapi.io/latest?base=' + base);
-        return data ? {rates: data.rates, base: data.base} : false;
-    } catch (error) {
-        console.error(error);
-    }
+export async function getExchange(base = "USD") {
+  try {
+    const { data } = await axios.get(
+      `http://api.exchangeratesapi.io/v1/latest?base=${base}&access_key=6a869760ead7263759b3cb09450189cb`
+    );
+    return data ? { rates: data.rates, base: data.base } : false;
+  } catch (error) {
+    console.error(error);
+  }
 }
